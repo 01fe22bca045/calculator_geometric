6b1de5883ffb8a27e93c707e8bb8b307feb4ad14
EvelynJessica0906 <01fe22bca045@kletech.ac.in>
2024-02-19 16:37:26 +0530

    extended code with geometric operations

---
+++ geometric.java
@@ -0,0 +1,12 @@
+// File: geometric.java
+package calci.calculator_geometric;
+
+public class geometric {
+    public double area_of_triangle(float base, float height) {
+        return 0.5 * base * height;
+    }
+
+    public double area_of_rectangle(float length, float width) {
+        return length * width;
+    }
+}
